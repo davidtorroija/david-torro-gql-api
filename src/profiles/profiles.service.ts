50b83fe6dc7465da6eb0973369251a30335bc240
David <davidtorroija@gmail.com>
2024-06-20 09:28:35 -0500

    feat: creating the service to get profiles

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { Profile } from 'src/graphql';
 
+const myProfile: Profile = {
+  firstName: 'David',
+  lastName: 'Torroija',
+  id: '1',
+  birthday: '01-10-1985',
+};
 @Injectable({})
 export class ProfilesService {
-  private profiles: Profile[] = [];
+  private profiles: Profile[] = [myProfile];
 
   getProfile(profileId: string) {
     const profile = this.profiles.find((p) => p.id === profileId);
     if (profile) {
       return profile;
     }
+  }
+
+  getProfiles() {
+    return this.profiles;
   }
 
   //   createProfile;
